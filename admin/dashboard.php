7629549bd3a0b144a37865ffb6f14986073954b7
Felix <felix.hanser@icloud.com>
2017-10-29 23:01:22 +0100

    Dashboard Admin

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Adminbereich Webprojekt</title>
+    <title>Adminbereich</title>
     <link href="css/style-admin.css" type="text/css" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
