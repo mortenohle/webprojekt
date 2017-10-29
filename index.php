ddecd2987ed4622c5483b4f4b3ae6619f0f43a77
Felix <felix.hanser@icloud.com>
2017-10-29 16:37:15 +0100

    Startseite

--- index.php
+++ index.php
@@ -1,12 +1,103 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Morten
- * Date: 29.10.17
- * Time: 14:27
- */
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Webprojekt Onlineshop</title>
+    <link href="css/style.css" type="text/css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="js/script.js"></script>
+    <script src="js/slider.js"></script>
+</head>
 
+<body>
+<header class="transition">
+    <div class="inner-wrapper">
+        <div class="nav-left">
+            <nav>
+                <ul class="transition">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="index.php?page=category&category=all">Kategorien</a>
+                        <ul>
+                            <li><a href="index.php?page=category&category=shirts">Shirts</a></li>
+                            <li><a href="index.php?page=category&category=pullover">Pullover</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="index.php?page=about">Über uns</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="logo-container">
+            <span class="logo">Logo</span>
+        </div>
+        <div class="nav-right">
+            <ul class="">
+                <li>Einloggen</li>
+                <li class="warenkorb"><img src="images/warenkorb.svg" alt="Warebkorb"></li>
+                <li><img src="images/suche.svg" alt="Suche" class="search-icon"></li>
+            </ul>
+        </div>
+    </div>
+</header>
 
-?>
+<?php if (!isset($_GET["page"]) ) { ?>
 
-<h1>Hallo Welt!</h1>
+    <div id="main-slider">
+        <ul>
+            <li style="background-image: url(images/slide_01.jpg);">
+                <div class="inner-wrapper">
+                    <div class="slide-content">
+                        <span class="slide-heading">All Black</span>
+                        <p>Ausgewählte und hochwertige Designerstücke.<br>Entdecke unsere neue All Black Kollektion.</p>
+                        <a class="btn-link" href="#">Mehr erfahren</a>
+                    </div>
+                </div>
+            </li>
+            <li style="background-image: url(images/slide_02.jpg);">
+                <div class="content"><a href="http://imageslidermaker.com">Slide Two</a></div>
+            </li>
+            <li style="background-image: url(images/slide_03.jpg);">
+
+            </li>
+        </ul>
+        <div class="slide-nav prev transition"><i class="fa fa-angle-left" aria-hidden="true"></i></div>
+        <div class="slide-nav next transition"><i class="fa fa-angle-right" aria-hidden="true"></i></div>
+    </div><!-- main-slider -->
+
+<?php } ?>
+
+<div id="content">
+    <div class="inner-wrapper content">
+
+        <?php
+
+        if (isset($_GET["page"]) ) {
+
+            switch ($_GET["page"]) {
+                case "category":
+                    include "category/index.php";
+                    break;
+                case "about":
+                    include "about/index.php";
+                    break;
+                default:
+                    include "start.php";
+                    break;
+            }
+        }
+        else
+        {
+            include "start.php";
+        }
+
+        ?>
+
+    </div>
+</div>
+
+<footer>
+    <p>Footer Webprojekt</p>
+</footer>
+
+</body>
+</html>
