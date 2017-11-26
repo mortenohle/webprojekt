4e840b54226f8d34d48bbae98e707a0bf4627c43
Morten <morten.ohle@icloud.com>
2017-11-26 23:16:09 +0100

    Warenkorb Mengen Update

--- cart/show.php
+++ cart/show.php
@@ -5,22 +5,104 @@
  * Date: 06.11.17
  * Time: 12:02
  */
+include_once('db/connect.php');
 
-if (isset($_POST["product"]) && $_POST["quantity"] > 0 ) {
+$test_cart = array(1 => 2);
+$cart_items = $test_cart;
+// $cart_items = $_SESSION["cart"];
 
-    $product_id = $_POST["product"];
-    $qty = $_POST["quantity"];
+$cart_count = count($cart_items);
+$cart_ids = "";
+foreach ($cart_items  AS $prod_id => $prod_qty) {
 
-    if (isset($_SESSION["cart"])) {
-
-        array_push($_SESSION["cart"], $product_id => $qty);
-
-
-    } else {
-
-        $_SESSION["cart"] = array($product_id => $qty);
-
+    if ($i == $cart_count - 1) {
+    $cart_ids .= "'". $prod_id ."')";
     }
+    else {
 
-
+        $cart_ids .= "'". $prod_id ."', ";
+    }
 }
+?>
+<form action="index.php?page=cart&cart=update">
+<div class="cartbox">
+
+    <div class="cart_header">
+
+    <div class="box">
+        <span class="cartlabel">Löschen</span>
+    </div>
+    <div class="box">
+    <span class="cartlabel">Bild</span>
+    </div>
+    <div class="box">
+        <span class="cartlabel">Beschreibung</span>
+    </div>
+    <div class="box">
+        <span class="cartlabel">Preis</span>
+    </div>
+    <div class="box">
+        <span class="cartlabel">Menge</span>
+    </div>
+    <div class="box">
+        <span class="cartlabel">Gesamt</span>
+    </div>
+    </div>
+
+
+<?php
+$sql_for_cart = "SELECT * FROM products WHERE id IN (".$cart_ids;
+foreach ($con->query($sql_for_cart) as $row) {
+    if (!empty($row['img'])) {
+        $imgurl = $row['img'];}
+    else {
+        $imgurl = "placeholder.jpg";
+    }
+    $totalprice += $row['price'] * $cart_items[$row['id']];
+    echo "
+    <div class=\"cart_row\">
+    <div class=\"box\">
+        <a href='index.php?page=cart&cart=delete&id=.".$row['id']."' class=\"cart_delete vertical_align_middle\">X</a>
+    </div>
+    <div class=\"box\">
+        <img class=\"cart_image\" src='images/products/".$imgurl."' alt='product placeholder'>
+    </div>
+    <div class=\"box\">
+        <span class=\"cart_desc vertical_align_middle \">".$row['name']."</span>
+    </div>
+    <div class=\"box\">
+        <span class=\"cart_price vertical_align_middle \">".$row['price']." €</span>
+    </div>
+    <div class=\"box\">
+    <input type='hidden' name='prod_id[]' value='".$row['id']."'>
+        <input class=\"quantity vertical_align_middle \" type=\"number\" name=\"quantity[]\" min=\"1\" max=\"9\" step=\"1\" value=\"".$cart_items[$row['id']]."\">
+    </div>
+    <div class=\"box\">
+        <span class=\"cart_price vertical_align_middle \">".$row['price'] * $cart_items[$row['id']]." €</span>
+    </div>
+    </div>";
+}
+?>
+
+    <div class="cart_footer">
+        <div class="box">
+        <input class="coupon_code cart_coupon" type="text" name="coupon_code" placeholder="Code eingeben">
+        <input class="cart_coupon" type="submit" value="Einlösen" name="coupon">
+        </div>
+        <div class="box">
+        <input class="cart_update" type="submit" value="Aktualisieren" name="update" />
+        </div>
+    </div>
+
+</div>
+
+    <div class="cart_summary">
+<h2>Warenkorb Summe</h2>
+<div class="summary_table">
+
+
+</div>
+        <input class="cart_update" type="submit" value="Kasse" name="checkout">
+    </div>
+
+</form>
---
+++ cart/update.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: morten
+ * Date: 19.11.17
+ * Time: 19:58
+ */
+
+if($_POST["update"]) {
+
+    $prod_id = $_POST['prod_id'];
+    $new_qty = $_POST['quantity'];
+
+    foreach( $prod_id as $key => $id ) {
+        $_SESSION["cart"][$id] = $new_qty[$key];
+    }
+}
+if($_POST["coupon"]) {
+
+    echo "Aktuell ist die Gutschein Funktion inaktiv.";
+
+}
+if($_POST["checkout"]) {
+
+}
