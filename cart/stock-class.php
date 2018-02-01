b9db226ca3e7e09247ac05b494c39a4c232debdc
Morten <morten.ohle@icloud.com>
2018-02-01 14:50:51 +0100

    Fixed Stock Management Class

---
+++ cart/stock-class.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: morten
+ * Date: 30.01.18
+ * Time: 09:43
+ */
+
+class stockmanagement  {
+
+    private $conn;
+
+    public function __construct($con) {
+        $this->conn = $con;
+    }
+
+
+    public function isavaible($id, $qty, $size) {
+
+        $statement = $this->conn->prepare("SELECT * FROM stock WHERE product_id = :id");
+        $statement->bindParam(':id', $id);
+        $statement->execute();
+        $result2 = $statement->fetch();
+
+        if ($result2[$size] >= $qty) {
+            $avaible = true;
+        } else { $avaible = false;}
+
+        return $avaible;
+    }
+
+    public function reducestock($id, $qty, $size) {
+
+        $statement = $this->conn->prepare("UPDATE stock SET :size = size - :qty WHERE product_id = :id ");
+        $statement->bindParam(':size', $size);
+        $statement->bindParam(':qty', $qty);
+        $statement->bindParam(':id', $id);
+        $statement->execute();
+
+    }
+}
