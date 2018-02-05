913678bfa0bef8989fd10ad430a7b98c8b72a65c
juliaseifert <js326@hdm-stuttgart.de>
2018-02-05 16:10:38 +0100

    Teamseite und Style Nutzerprofil und Über uns

--- about/index.php
+++ about/index.php
@@ -1,2 +1,49 @@
-<h1>Über uns</h1>
-<p>Das sind wir.</p>
+<h1 class="center">Über uns</h1>
+<span class="h1_sub">Starke Marken sind Teamwork.</span>
+<div class='boxcontainer'>
+
+    <div class='contentbox'>
+        <img src="images/team_julia.jpg" class="team"alt="Team Julia">
+        <h3>Julia Seifert</h3>
+        <h4>Event/Geschäftsführung</h4><br>
+        <p> Julia begleitet "LOGO" seit der ersten Stunde. Sie leitet heute das
+            Unternehmen als Geschäftsführerin, ist verantwortlich für die
+            Bereiche Retail, B2B, Logistik, Event und Messeplanung.
+            In ihrer Freizeit erkundigt sie gerne ferne Länder.</p>
+    </div>
+
+    <div class='contentbox'>
+        <img src="images/team_felix.jpg" class="team" alt="Team Felix">
+        <h3>Felix Hanser</h3>
+        <h4>CTO (Chief Technology Officer)</h4><br>
+        <p> Felix trägt als CTO (Chief Technology Officer
+            die Verantwortung für alle technischen Ressourcen
+            und optimiert interne Prozesse. Sein Herz schlägt für
+            leistungsfähige Automobilfahrzeuge. In seiner Freizeit macht er
+            die Downhillstrecken mit seinem Mountainbike im Schwarzwald unsicher.
+        </p>
+    </div>
+
+    <div class='contentbox'>
+        <img src="images/team_morten.jpg" class="team" alt="Team Morten"/>
+        <h3>Morten Ohle</h3>
+        <h4>Finanzen/Controlling</h4><br>
+        <p>Morten ist verantwortlich für die Finanzen und das Controlling.
+            Er ist leidenschaftlicher Autombilliebhaber und beschäftigt sich in seiner Freizeit
+            mit der Restaurierung von alten Oldtimern. Nebenbei trainiert er seine
+            Kraftausdauer im Sportstudio.</p>
+    </div>
+
+
+
+    <div class='contentbox'>
+        <img src="images/team_fatma.jpg" class="team" alt="Team Fatma">
+        <h3>Fatma El air</h3>
+        <h4>CEO/Creative Director</h4><br>
+        <p>Fatma ist CEO und Creative Director von "LOGO". Sie entwickelt neue Designs
+            und definiert die Markenidentität sowie Außenkommunikation.
+            Außerdem ist er für alle Marketingmaßnahmen und Kooperationen zuständig.
+            In ihrer Freizeit ist Fatma leidenschaftliche Kite-Surfering und
+            ist so oft wie möglich am Wasser.</p>
+    </div>
+</div>
