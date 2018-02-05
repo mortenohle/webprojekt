cabb904089146341e4b46f87e68625a41c160d2a
felixhanser <felix.hanser@icloud.com>
2018-02-05 17:55:40 +0100

    User Profile Sidenav

---
+++ account/sidenav-profile.php
@@ -0,0 +1,11 @@
+<div class="kundenkonto_menu_toggle">
+    Mein Profil
+</div>
+<div class="menu_kundenkonto">
+<ul class="sidenav_kundenkonto">
+    <li class="user-profile"><a href="index.php?page=account&action=myprofile">Übersicht</a></li>
+    <li class="user-orders"><a href="index.php?page=account&action=myorders">Meine Bestellungen</a></li>
+    <li class="user-edit"><a href="index.php?page=account&action=profileedit">Meine Daten ändern</a></li>
+    <li class="user-logout"><a href="index.php?page=account&action=logout" class="logout">Abmelden</a></li>
+</ul>
+</div>
